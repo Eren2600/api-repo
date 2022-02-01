40c327a9283d6d798bfe4e8221cf7a501dcccb63
Eren2600 <e.kolac@jarvis-it.de>
2022-02-01 16:09:45 +0100

    PHP

---
+++ meteo-api.php
@@ -0,0 +1,8 @@
+/**
+* Plugin Name:          meteocontrol API
+* Plugin URI:           https://plugin.jarvis-it.de/meteo-api/
+* Description:          Binde die meteocontrol API auf deine Webseite ein.
+* Version:              0.0.1
+* Author:               Eren Kolac
+* License:              GPL v2 oder neuer
+ */
